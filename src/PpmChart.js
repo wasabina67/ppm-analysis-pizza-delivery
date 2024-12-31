18a68718aac1f23bf23070d63bfe3d964a74689b
wasabina67 <wasabina67@gmail.com>
2025-01-01 08:53:23 +0900

    Update src/PpmChart.js

--- src/PpmChart.js
+++ src/PpmChart.js
@@ -4,7 +4,22 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 const PpmChart = () => {
-  const data = {};
+  const data = {
+    datasets: [
+      {
+        label: "Areas",
+        data: [
+          { x: 20, y: 80, label: "Area A" },
+          { x: 80, y: 80, label: "Area B" },
+          { x: 20, y: 20, label: "Area C" },
+          { x: 80, y: 20, label: "Area D" },
+        ],
+        backgroundColor: "rgba(75, 192, 192, 1)",
+      },
+    ],
+  };
+
+  const options = {};
 
   return <div>PpmChart</div>;
 };
