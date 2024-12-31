d47f5b5207258941e4dcd3edf560eacf18cf4e00
wasabina67 <wasabina67@gmail.com>
2025-01-01 00:04:06 +0900

    Update src/PpmChart.js

--- src/PpmChart.js
+++ src/PpmChart.js
@@ -1,4 +1,11 @@
+import { Scatter } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
 const PpmChart = () => {
+  const data = {};
+
   return <div>PpmChart</div>;
 };
 
